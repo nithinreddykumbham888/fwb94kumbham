528c968b50e05b2b4aa6f92c3b21df6965b8cc5c
Nithin Reddy Kumbham <nithinreddykumbham@gmail.com>
2021-11-02 23:31:27 -0500

    Updated files

--- routes/addmods.js
+++ routes/addmods.js
@@ -13,15 +13,26 @@ module.exports = router;
 
 
 
-// 7) Express makes it easy to get any query parameters that were added to the url. If req is 
-// the request argument and you are looking for the parameter foo, you can get the value 
-// of the parameter or some null like response by doing req.query.foo 
-// 8) Inside the get router for addmods do the following to report the query values   let query = req.query 
-// console.log(`rows ${query.rows}`) 
-// console.log(`cols ${query.cols}`) 
- 
-// And then to pass the query object into the pug engine, use   { title: ‘Addmods’, query: query}  
-// As the second argument of the render. 
-// 9) Restart the local heroku server and verify that the new endpoint works.  Try it with 
-// ?rows=10&cols=3 as an example and see if the console records the correct values. 
-// 10) Add, Commit and Push to origin main. 
+// Part 7: Working with query strings in the pug engine 
+// 1) Our goal is to make a table with the given number of rows and columns where each 
+// cell adds the row and column and mods by the number of rows. We use 0 based 
+// indexing on row/col number.  For a 4 by 5 we expect 
+// 0 1 2 3 0 
+// 1 2 3 0 1 
+// 2 3 0 1 2  
+// 3 0 1 2 3 
+// 2) We passed the query arguments into pug, and the values will always be strings.  So we 
+// need to do a conversion. 
+// 3) Do a paragraph where you show the values of query.rows and query.cols. Use #{} 
+// string interpolation. 
+// 4) Do two unbuffered lines where you set rows to Number(query.rows) and col to 
+// Number(query.cols).  Number will do its best to convert the string to a number.  The 
+// result could be a NaN, which surprisingly is a number. 
+// 5) We now want to kick out anything which is not an integer.  We start with an 
+// unbuffered if (no need to put a -) and check to see if we don’t have an integer. To see 
+// if row is an integer, we can use the condition Number.isInteger(rows). If either rows or 
+// cols is not an integer do a paragraph and report the problem. 
+// 6) We want to kick out and values that are too big or too small.  We are going to restrict 
+// ourself to rows/cols in the range of 1 to 6. 
+// 7) Use an unbuffered else if to check for rows out of range. Do a paragraph if this is the 
+// case and report the issue.  
